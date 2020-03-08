a0641dbb6a423ef64384f5cb1f93930be03bef1b
Roger Mikomägi <43129529+rogertht@users.noreply.github.com>
2020-03-08 21:53:19 +0200

    esimene ports

---
+++ before_html.js
@@ -0,0 +1,27 @@
+var names_of_days = new Array(7);
+    names_of_days =
+    [
+        "PÜHAPÄEV"  ,
+        "ESMASPÄEV" ,
+        "TEISIPÄEV" ,
+        "KOLMAPÄEV" ,
+        "NELJAPÄEV" ,
+        "REEDE"     ,
+        "LAUPÄEV"            
+    ]
+var names_of_months = new Array(7);
+    names_of_months =
+    [
+        "JAANUAR"   ,
+        "VEEBRUAR"  ,
+        "MÄRTS"     ,
+        "APRILL"    ,
+        "MAI"       ,
+        "JUUNI"     ,
+        "JUULI"     ,
+        "AUGUST"    ,
+        "SEPTEMBER" ,
+        "OKTOOBER"  ,
+        "NOVEMBER"  ,
+        "DETSEMBER"
+    ]
