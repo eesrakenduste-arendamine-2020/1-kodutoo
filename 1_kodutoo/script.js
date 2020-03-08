c26535a03be107a03a1b7762c60430324dd0fa5c
Henri Mägi <55129119+henrimag@users.noreply.github.com>
2020-03-08 21:00:41 +0200

    väike muutus

--- 1_kodutoo/script.js
+++ 1_kodutoo/script.js
@@ -89,18 +89,18 @@ function updateClock(){
 
  setInterval(setKell, 1000)
 
-const hourSeier = document.querySelector('[data-hour-seier]')
-const minuteSeier = document.querySelector('[data-minute-seier]')
-const secondSeier = document.querySelector('[data-second-seier]')
+const hourHand = document.querySelector('[data-hour-hand]')
+const minuteHand = document.querySelector('[data-minute-hand]')
+const secondHand = document.querySelector('[data-second-hand]')
 
 function setKell() {
     const currentDate = new Date()
     const secondsRatio = currentDate.getSeconds() / 60
     const minutesRatio = (secondsRatio + currentDate.getMinutes()) / 60
     const hoursRatio = (minutesRatio + currentDate.getHours()) / 12
-    setRotation(secondSeier, secondsRatio)
-    setRotation(minuteSeier, minutesRatio)
-    setRotation(hourSeier, hoursRatio)
+    setRotation(secondHand, secondsRatio)
+    setRotation(minuteHand, minutesRatio)
+    setRotation(hourHand, hoursRatio)
 }
 
 function setRotation(element, rotationRatio) {
